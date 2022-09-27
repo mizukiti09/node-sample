0b8b807f5fd20ef40ec062e5704aeed16ba9714a
mizukiti09 <gorigoritikitiki@gmail.com>
2022-09-27 20:21:39 +0900

    トランザクションの利用(実装まで）

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const app = express();
 app.set("view engine", "ejs");
 app.disable("x-powered-by");
 
+// Expose global method to view engine
+app.use((req, res, next) => {
+    res.locals.moment = require("moment");
+    res.locals.padding = require("./lib/math/math.js").padding;
+    next();
+});
+
 // Static resource rooting.
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
@@ -19,12 +26,15 @@ app.use("/public", express.static(path.join(__dirname, "/public")));
 app.use(accesslogger());
 
 // Dynamic resource rooting.
+app.use("/search", require("./routes/search.js"));
+app.use("/shops", require("./routes/shops.js"));
 app.use("/", require("./routes/index.js"));
+
 
 // Set application log.
 app.use(applicationlogger());
 
 // Execute web application.
 app.listen(PORT, () => {
-  logger.application.info(`Application listening at :${PORT}`);
+    logger.application.info(`Application listening at :${PORT}`);
 });
